b7c49d84b1c7bfae1dc2732df5b190de12167ec8
Logan Riley <rileylog@gmail.com>
2013-10-17 00:22:43 -0600

    setting up mailer template that which the html came from mailerchimp.com. Also changing around the contact page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@myportfolio.com"
 
+  layout 'email'
+
   def welcome(user)
     @user = user
     mail(to: @user.email, subject: "#{user.email}, welcome to my portfolio")
