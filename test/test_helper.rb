10c1e344d36ba6b39842f7a6e53c41fafa8ad546
Logan Riley <rileylog@gmail.com>
2013-10-22 22:15:19 -0600

    adding some testing gem and such

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+require "minitest/spec"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
@@ -11,8 +12,6 @@ require "minitest/rails"
 require "minitest/pride"
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   include FactoryGirl::Syntax::Methods
-
   # Add more helper methods to be used by all tests here...
 end
